91e50bb7c334e50d510252552915a828fe2c5e7e
Salvador González <zyrian@mylocker.dev>
2025-01-20 05:29:22 -0700

    Tests basics

--- basics/08-test.py
+++ basics/08-test.py
@@ -8,3 +8,17 @@
 # "Too high" if the guess is greater than the number.
 # "Too low" if the guess is less than the number.
 # End the game when the user guesses correctly and display the number of attempts.
+import random
+
+counter = 0
+rng = random.randint(1,100)
+print(rng)
+
+usernum = int(input("Guess the number I'm thinking!: "))
+if usernum == rng:
+    print("You gessed the number, you're a wizard Harry!")
+elif usernum >= rng:
+    print("Too high")
+else:
+    print("Too low")
+counter +=1
